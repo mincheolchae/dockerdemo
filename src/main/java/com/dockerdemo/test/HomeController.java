616352d8d7a03bf19ef5638105218c96cfdccba8
INFRAWARE\dennis.m.chae <dennis.m.chae@polarisoffice.com>
2022-05-11 14:43:35 +0900

    test

--- src/main/java/com/dockerdemo/test/HomeController.java
+++ src/main/java/com/dockerdemo/test/HomeController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
@@ -26,4 +27,12 @@ public class HomeController {
 		logger.info( "test mapping.." );
 		return "test";
 	}
+
+	@ResponseBody
+	@GetMapping("/sample")
+	public String sample() {
+		logger.info( "sampling.." );
+		return "SAMPLE!!";
+	}
+
 }
