74f804dc70651948681afcb1f6a62f1fa93a45dc
INFRAWARE\dennis.m.chae <dennis.m.chae@polarisoffice.com>
2022-05-11 13:35:12 +0900

    test

--- src/main/java/com/dockerdemo/HomeController.java
+++ src/main/java/com/dockerdemo/HomeController.java
@@ -1,13 +1,23 @@
 package com.dockerdemo;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+	private final Logger logger = LoggerFactory.getLogger( getClass() );
 
 	@GetMapping("/")
 	public String home() {
-		return "home";
+		logger.info( "here i am.." );
+		return "index";
 	}
 }
